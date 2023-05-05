538fe103d21b9f237089d93a82922478688fce8a
vHawk <48140945+StrandedKitty@users.noreply.github.com>
2023-05-05 16:11:08 +0200

    Fix code style

--- src/lib/tile-processing/vector/qualifiers/factories/helpers/getPitchTypeFromTags.ts
+++ src/lib/tile-processing/vector/qualifiers/factories/helpers/getPitchTypeFromTags.ts
@@ -12,11 +12,9 @@ export default function getPitchTypeFromTags(tags: Record<string, string>): Vect
 
 	for (const value of values) {
 		const type = lookup[value];
-		
-		if (type == 'basketball') {
-			if( getTagValues(tags, 'hoops')[0] == '1') {
-				return 'generic';
-			}
+
+		if (type === 'basketball' && getTagValues(tags, 'hoops')[0] == '1') {
+			return 'generic';
 		}
 
 		if (type !== undefined) {
