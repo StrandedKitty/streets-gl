c0cd3c4c34414273c807731edbb1a8ae5220dde8
StrandedKitty <48140945+StrandedKitty@users.noreply.github.com>
2023-05-31 23:34:24 +0200

    Support any tunnel tag values

--- src/lib/tile-processing/vector/qualifiers/factories/helpers/isUnderground.ts
+++ src/lib/tile-processing/vector/qualifiers/factories/helpers/isUnderground.ts
@@ -1,10 +1,12 @@
 import {readTagAsFloat} from "~/lib/tile-processing/vector/qualifiers/factories/helpers/tagHelpers";
 
 export default function isUnderground(tags: Record<string, string>): boolean {
+	const isInTunnel = tags.tunnel !== undefined && tags.tunnel !== 'no';
+
 	return (
 		tags.location === 'underground' ||
 		readTagAsFloat(tags, 'level') < 0 ||
-		tags.tunnel === 'yes' ||
+		isInTunnel ||
 		tags.parking === 'underground'
 	);
 }
